2613a85fdb091298623a53954a862377fe1736f5
Xin Vance <xinpengfei520@gmail.com>
2022-08-21 12:16:05 +0800

    update

---
+++ Enesco/Controllers/Search/SearchMusicViewController.h
@@ -0,0 +1,17 @@
+//
+//  SearchMusicViewController.h
+//  Enesco
+//
+//  Created by PengfeiXin on 2021/7/10.
+//  Copyright © 2021 aufree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchMusicViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Enesco/Controllers/Search/SearchMusicViewController.m
@@ -0,0 +1,32 @@
+//
+//  SearchMusicViewController.m
+//  Enesco
+//
+//  Created by PengfeiXin on 2021/7/10.
+//  Copyright © 2021 aufree. All rights reserved.
+//
+
+#import "SearchMusicViewController.h"
+
+@interface SearchMusicViewController ()
+
+@end
+
+@implementation SearchMusicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
